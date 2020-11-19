a5c8a3e015b59ec63733d7dd313317bdf586b99d
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-18 18:10:13 -0700

    Slices

--- src/main.rs
+++ src/main.rs
@@ -110,9 +110,23 @@ fn arrays() {
     }
 }
 
+fn use_slice(slice: &mut[i32]) {
+    println!("first elem = {}, len = {}", slice[0], slice.len());
+    slice[0] = 4321;
+}
+
+fn slices() {
+    let mut data = [1,2,3,4,5];
+
+    use_slice(&mut data[1..4]);
+    // use_slice(&mut data);
+    println!("{:?}", data);
+}
+
 fn main() {
     //structures();
     //enums();
     //unions();
-    arrays();
+    // arrays();
+    slices();
 }
