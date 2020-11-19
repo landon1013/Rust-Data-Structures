061b01e605c2c3e3e5fc004fc283604a4b1c8f58
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-18 17:23:30 -0700

    Arrays

--- src/main.rs
+++ src/main.rs
@@ -73,8 +73,46 @@ fn unions() {
     process_value(IntOrFloat{i:5});
 }
 
+fn arrays() {
+    let mut a:[i32;5] = [1,2,3,4,5];
+
+    println!("a has {} elements, first is {}",
+        a.len(), a[0]);
+    a[0] = 321;
+    println!("a[0] = {}", a[0]);
+
+    println!("{:?}", a);
+
+    if a != [1,2,3,4,5] {
+        println!("does not match");
+    }
+
+    let b = [1; 10];
+    for i in 0..b.len() {
+        println!("{}", b[i]);
+    }
+
+    println!("b took up {} bytes", mem::size_of_val(&b));
+
+    let mtx:[[f32;3];2] =
+    [
+        [1.0, 0.0, 0.0],
+        [0.0, 2.0, 0.0]
+    ];
+    println!("{:?}", mtx);
+
+    for i in 0..mtx.len() {
+        for j in 0..mtx[i].len() {
+            if i == j {
+                println!("mtx[{}][{}] = {}", i, j, mtx[i][j]);
+            }
+        }
+    }
+}
+
 fn main() {
     //structures();
     //enums();
-    unions();
+    //unions();
+    arrays();
 }
